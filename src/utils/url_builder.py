2f9ede457fa77982d232dfbc6708f1170fac184b
NatanielSlo <nataniel.slowikowski@gmail.com>
2026-01-10 18:43:01 +0100

    Fixed issue with 1st page not reading correctly

--- src/utils/url_builder.py
+++ src/utils/url_builder.py
@@ -3,7 +3,9 @@ class UrlBuilder:
         pass
 
     def build_search_url(self, query, page_num=1, sort=None, filters=None,phone_model=None):
-        if phone_model: 
+        if phone_model:
+            if page_num == 1:
+                url = f"https://www.olx.pl/elektronika/q-{query}/?search%5Bfilter_enum_phonemodel%5D%5B0%5D={phone_model}&search%5Border%5D=created_at%3Adesc"
             url = f"https://www.olx.pl/elektronika/q-{query}/?page={page_num}&search%5Bfilter_enum_phonemodel%5D%5B0%5D={phone_model}&search%5Border%5D=created_at%3Adesc"
         else: 
             url = f"https://olx.pl/q-{query}/?page={page_num}"
